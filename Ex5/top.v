bdf57bf4d64c2e1eb502f7941100009beac37ab6
Cloud User <centos@ip-172-31-7-42.eu-west-2.compute.internal>
2021-06-09 15:58:46 +0000

    Solution to ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,47 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+module AC (
+    //add ports 
+	input clk,
+	input [4:0] temperature,
+	output heating,
+	output cooling
+    );
+                    
+    //add registers and wires
+	reg heating;
+	reg cooling;
 
+    //add user logic
+	initial begin
+	cooling = 0;
+	heating = 0;
+	end
+
+	always @ (posedge clk) begin
+
+	if (!(heating)&&(!(cooling))) begin
+		if ((temperature > 22)||(temperature == 22)) begin
+		cooling <= 1;
+		end
+		if ((temperature < 18)||(temperature == 18)) begin
+		heating <= 1;
+		end
+	end
+
+	else if ((heating)&&(!(cooling))) begin
+		if ((temperature > 20)||(temperature == 20)) begin
+		heating <= 0;
+		end
+	end
+
+	else if (!(heating)&&(cooling)) begin
+		if ((temperature < 20)||(temperature == 20)) begin
+		cooling <= 0;
+		end
+	end
+	
+	end
+      
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,72 @@
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Parameters
+	parameter CLK_PERIOD = 5;
+//Regitsers and wires
+	reg clk;
+	reg [4:0] temperature;
+	wire heating;
+	wire cooling;
+	reg err;
+	
+//Clock generation
+initial begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//User logic
+
+	initial begin
+	#10
+	err = 0;
+	temperature = 16;
+
+	
+	forever begin
+	#20
+	if ((heating)&&(cooling)) begin
+	err = 1;
+	end
+	
+	if (temperature < 26) begin
+	temperature = temperature + 2;
+
+	end
+
+	else begin
+	forever begin
+	#20
+	if ((heating)&&(cooling)) begin
+	err = 1;
+	end
+	temperature = temperature - 2;
+	end
+	end
+
+	end
+	end
+
+
+//Finish test, check for success
+	initial begin
+	#300
+	if (err == 0) begin
+	$display("***TEST PASSED***");
+	end
+	else begin
+	$display("***TEST FAILED***");
+	end
+	$finish;
+	end
+
+//Instantiate counter module
+	AC top(clk,temperature,heating,cooling);
+endmodule 
